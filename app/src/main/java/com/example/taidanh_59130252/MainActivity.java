df375707829a79b87f34194bb5ff93c240faf549
= <=>
2020-04-10 16:12:47 +0700

    V 1.0

---
+++ app/src/main/java/com/example/taidanh_59130252/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.taidanh_59130252;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
